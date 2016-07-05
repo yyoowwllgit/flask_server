f9c0ef82abaf293723053d08367951ba548cb42d
huang <1942461968@qq.com>
2016-07-05 22:19:22 +0800

    modify mail use smtp.sina.com

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ import os
 from flask_script import Manager,Command,Option
 from flask_mail import Mail,Message
 from test_col_diamon import app
-app.config['MAIL_SERVER']='smtp.163.com'
+app.config['MAIL_SERVER']='smtp.sina.com'
 app.config['MAIL_PORT']=25
 app.config['MAIL_USE_TLS']=True
 app.config['MAIL_USERNAME']=os.environ.get('mailusername')
